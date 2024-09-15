cb848b1b953b79a6031f2c79983888bd19f27761
evankelch <evan.c.kelch@gmail.com>
2024-09-15 13:58:02 -0400

    init commit

---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"time"
+
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/widget"
+)
+
+func main() {
+	a := app.New()
+	w := a.NewWindow("Hello world")
+
+	clock := widget.NewLabel("")
+	updateTime(clock)
+	w.SetContent(clock)
+	w.Resize(fyne.NewSize(200, 100))
+
+	go func() {
+		for range time.Tick(time.Second) {
+			updateTime(clock)
+		}
+	}()
+
+	w.ShowAndRun()
+}
+
+func updateTime(clock *widget.Label) {
+	formatted := time.Now().Format("Time: 03:04:05")
+	clock.SetText(formatted)
+}

a0ae627eb9d0ecf65191aaed1e9dc327659f9199
evankelch <evan.c.kelch@gmail.com>
2024-09-15 22:38:20 -0400

    placeholder UI without impl added

--- main.go
+++ main.go
@@ -1,30 +1,46 @@
 package main
 
 import (
-	"fmt"
 	"time"
 
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/theme"
 	"fyne.io/fyne/v2/widget"
 )
 
 func main() {
 	a := app.New()
-	w := a.NewWindow("Hello world")
+	w := a.NewWindow("AppContainer")
+	w.Resize(fyne.NewSize(1200, 700))
 
-	fmt.Print("test")
+	var songList = []string{"song1", "song2", "song3", "song4", "song5", "song6", "song7", "song8", "song9", "song10", "song11", "song12", "song13", "song14", "song15", "song16", "song17", "song18"}
 
-	clock := widget.NewLabel("")
-	updateTime(clock)
-	w.SetContent(clock)
-	w.Resize(fyne.NewSize(200, 100))
+	scrollArea := widget.NewList(
+		func() int { return len(songList) },
+		func() fyne.CanvasObject { return widget.NewLabel("template") },
+		func(i widget.ListItemID, o fyne.CanvasObject) { o.(*widget.Label).SetText(songList[i]) })
 
+	previousBtn := widget.NewButtonWithIcon("", theme.Icon(theme.IconNameMediaSkipPrevious), func() {})
+	ppBtn := widget.NewButtonWithIcon("", theme.Icon(theme.IconNameMediaPause), func() {})
+	nextBtn := widget.NewButtonWithIcon("", theme.Icon(theme.IconNameMediaSkipNext), func() {})
+	controlArea := container.NewHBox(previousBtn, ppBtn, nextBtn)
+	controlArea.Resize(fyne.Size{Width: 300, Height: 150})
+
+	progressBar := widget.NewProgressBar()
 	go func() {
-		for range time.Tick(time.Second) {
-			updateTime(clock)
+		songDur := 180
+		for i := 1; i < songDur; i++ {
+			time.Sleep(time.Second)
+			progressBar.SetValue(float64(i) / float64(songDur))
 		}
 	}()
+
+	controlGroup := container.NewVBox(controlArea, progressBar)
+
+	content := container.NewBorder(nil, controlGroup, nil, nil, scrollArea)
+	w.SetContent(content)
 
 	w.ShowAndRun()
 }
